7b1da85ade24269c175dbe19e0c968b1e7c32425
luzmiranda-foton <luzmirandatilano.h@gmail.com>
2026-04-27 23:12:51 -0600

    main.py

--- main.py
+++ main.py
@@ -35,8 +35,8 @@ if materia == "🏠 Inicio":
     st.image("https://images.unsplash.com/photo-1434030216411-0b793f4b4173?auto=format&fit=crop&q=80&w=1000", caption="¡Mantén el enfoque!")
 elif materia == "Legislacion":
     try:
-    st.switch_page("pages/Legislacion.py")
-except:
-    st.switch_page("Pages/Legislacion.py")
+        st.switch_page("pages/Legislacion.py")
+    except:
+        st.switch_page("Pages/Legislacion.py")
 
 
